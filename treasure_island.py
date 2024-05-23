5cddff3a6de8aeb89a6e875bc913ede8413d712f
mazzi2023 <mazilugabriel2009@gmail.com>
2024-05-23 14:06:27 +0300

    .

--- treasure_island.py
+++ treasure_island.py
@@ -1,17 +1,17 @@
 print("Welcome to Treasure Island. Your mission is to find the treasure.")
-user_choise = input("You're at a cross road. Where do you want to go? Type left or right \n")
+user_choise = input("You're at a cross road. Where do you want to go? Type left or right \n").lower()
 
 if user_choise == "right":
-    user_choise2 = input("Do you want to swimm or to wait? \n")
+    user_choise2 = input("Do you want to swimm or to wait? \n").lower()
     if user_choise2 == "wait":
-            user_choise3 = input("Which door? Red, Yellow or Blue?")
+            user_choise3 = input("Which door? Red, Yellow or Blue? \n").lower()
             if user_choise3 == "Red":
                 print("Burned by fire. Game Over.")
             elif user_choise3 == "Yellow":
                 print("You Win!")
             elif user_choise3 == "Blue":
                 print("Eaten by beasts. Game Over")
-            elif user_choise3 == "":
+            else:
                 print("Game Over")    
     else:
         print("Attacked by trout. Game over")
